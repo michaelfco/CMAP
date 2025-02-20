f71f684cf159cc7f867145d63d87a602fc8967bb
michaelfco <44820588+michaelfco@users.noreply.github.com>
2025-02-20 23:13:27 +0000

    add basic configs and files

---
+++ CMAPTask.Infrastructure/Extensions/ApplicationServiceExtension.cs
@@ -0,0 +1,30 @@
+﻿using CMAPTask.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMAPTask.Infrastructure.Extensions;
+
+public static class ApplicationServiceExtension
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+
+        services.AddDbContext<CMAPDbContext>(opt =>
+        {
+            //opt.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddDbContext<CMAPDbContext>(options =>
+options.UseSqlite("Data Source=CmapTask.db"));
+       
+        //services.AddScoped<IValidationService, ValidationService>();
+
+        return services;
+    }
+}
